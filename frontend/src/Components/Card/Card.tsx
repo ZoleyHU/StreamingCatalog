ef8ca21ae3fc4033f4d5c35839f6ccfcc99ff9c3
Hegedűs Zoltán <h045746@stud.u-szeged.hu>
2024-05-15 15:31:39 +0200

    Fix: Visual fixes, added season count for series

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -5,14 +5,16 @@ interface Props {
   id: string;
   show: Show;
 }
-//TODO: add stream options in a list next to each other
+//TODO: replace "hu" with a prop from the country search
+//TODO: add price and quality for rental/buyable movies, on their own page
 const Card: React.FC<Props> = ({id, show}: Props): JSX.Element => {
   return (
     <div className='card'>
       <img src={show.imageSet.verticalPoster.w240} alt={show.title + " poster"}/>
       <div className='details'>
-        <h2>{show.title}</h2>
+        <h2>{show.title + " (" + (show.releaseYear===undefined ? (show.firstAirYear +"-"+ show.lastAirYear) : show.releaseYear ) + ")"}</h2>
         <p>{show.showType}</p>
+        <p>{show.showType==='series' && show.seasonCount + " Seasons"}</p>
       </div>
     </div>
   )
