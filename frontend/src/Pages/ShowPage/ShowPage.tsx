25f5ef55bcd11ac6e0913bf66a8322555c6a7c60
Hegedűs Zoltán <h045746@stud.u-szeged.hu>
2024-05-22 12:19:33 +0200

    New: showpage fetches the shows data

--- frontend/src/Pages/ShowPage/ShowPage.tsx
+++ frontend/src/Pages/ShowPage/ShowPage.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import { Show } from 'streaming-availability';
+import { getShow } from '../../api';
 
 interface Props {}
 
 const ShowPage = (props: Props) => {
+    let {country, showId} = useParams();
+    const [show, setShow] = useState<Show>();
+
+    useEffect(() => {
+        const initShowData = async () => {
+            const result = await getShow(showId!, country!);
+            setShow(result);
+        }
+        initShowData();
+    }, [showId, country]);
+
   return (
-    <div>ShowPage</div>
+    <div>
+        <p>{show?.title}</p>
+        <img src={show?.imageSet.verticalPoster.w240} alt={show?.title} />
+    </div>
   )
 }
 
