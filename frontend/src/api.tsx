8f8bf48b4f43004e1b989354a50d6ca44d7953aa
Hegedűs Zoltán <h045746@stud.u-szeged.hu>
2024-05-22 12:17:08 +0200

    New: created api call to get a show

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -16,3 +16,19 @@ export const searchShows = async (title: string, country: string) => {
         return "An error has occured. Please try again later."
     }
 }
+
+export const getShow = async (id: string, country: string) => {
+    try {
+        const RAPID_API_KEY = process.env.REACT_APP_STREAMING_AVAILABILITY_API_KEY;
+        const client = new streamingAvailability.Client(new streamingAvailability.Configuration({
+            apiKey: RAPID_API_KEY
+        }));
+        const data = await client.showsApi.getShow({
+            id: id,
+            country: country,
+        });
+        return data;
+    } catch (error) {
+        console.log("error: ", error);
+    }
+}
