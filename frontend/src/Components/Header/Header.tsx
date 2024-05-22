5576e90f73c5d7408ea82e74c0d44dba98f490dd
Hegedűs Zoltán <h045746@stud.u-szeged.hu>
2024-05-22 11:42:46 +0200

    New: added links to different parts of the app

--- frontend/src/Components/Header/Header.tsx
+++ frontend/src/Components/Header/Header.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 interface Props {}
 
 const Header = (props: Props) => {
   return (
-    <div className="grid w-auto h-52 items-center text-center bg-gradient-to-b to-slate-600 via-slate-300 from-slate-600">
-      <div className='h-fit rounded-xl ml-[30%] mr-[30%] cursor-default border-solid border-2 border-black bg-slate-300'>
-        <p className='text-6xl leading-normal'>Streaming catalog</p>
+    <div className="grid w-full h-52 items-center text-center bg-gradient-to-b to-slate-600 via-slate-300 from-slate-600">
+      <div className='h-fit rounded-xl ml-[30%] mr-[30%] border-solid border-2 border-black bg-slate-300'>
+        <Link to="/">
+          <p className='text-6xl leading-normal'>Streaming catalog</p>
+        </Link>
       </div>
     </div>
   )
