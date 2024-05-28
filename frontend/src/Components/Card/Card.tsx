717b156aa8a570562e8a5015ae16a24199a17f09
Hegedűs Zoltán <h045746@stud.u-szeged.hu>
2024-05-28 13:56:03 +0200

    Fix: search result now open in new window

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -11,7 +11,7 @@ interface Props {
 //TODO: add price and quality for rental/buyable movies, on their own page
 const Card: React.FC<Props> = ({id, show, country}: Props): JSX.Element => {
   return (
-    <Link to={`/show/${country}/${show.imdbId}`} className='m-auto w-60 h-fit flex flex-col items-center text-center p-3 rounded-xl overflow-hidden shadow-lg shadow-slate-700 bg-white'>
+    <Link to={`/show/${country}/${show.imdbId}`} target="_blank" rel="noopener noreferrer" className='m-auto w-60 h-fit flex flex-col items-center text-center p-3 rounded-xl overflow-hidden shadow-lg shadow-slate-700 bg-white'>
       <img src={show.imageSet.verticalPoster.w240} alt={show.title + " poster"}/>
       <div>
         <p className='text-2xl'>{show.title + " (" + (show.releaseYear===undefined ? (show.firstAirYear +"-"+ show.lastAirYear) : show.releaseYear ) + ")"}</p>
