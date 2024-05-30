bd7814a23856ab8ded7c17fb741595a0c048e228
Hegedűs Zoltán <h045746@stud.u-szeged.hu>
2024-05-30 14:14:29 +0200

    New: added header to showPage, other css fixes

--- frontend/src/Pages/ShowPage/ShowPage.tsx
+++ frontend/src/Pages/ShowPage/ShowPage.tsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router'
 import { Show } from 'streaming-availability';
 import { getShow } from '../../api';
 import StreamingOptions from '../../Components/StreamingOptions/StreamingOptions';
+import Header from '../../Components/Header/Header';
 
 interface Props {}
 
@@ -18,13 +19,15 @@ const ShowPage = (props: Props) => {
         initShowData();
     }, [showId, country]);
 
-//TODO: add header
   return (
-    <div className='w-full h-full '>
-      <div className='w-4/5 m-auto bg-gray-500 flex flex-col items-center'>
-        <p>{show?.title}</p>
-        <img src={show?.imageSet.verticalPoster.w240} alt={show?.title} />
-        <StreamingOptions show={show!} country={country!}/>
+    <div className='min-h-screen bg-gradient-to-b from-slate-600 to-slate-700'>
+      <Header />
+      <div className='w-4/5 h-full m-auto bg-slate-600 flex flex-col items-center text-3xl font-bold'>
+          <p>{show?.title + " (" + (show?.releaseYear===undefined ? (show?.firstAirYear +"-"+ show?.lastAirYear) : show.releaseYear ) + ")"}</p>
+          <div className='flex flex-row font-normal m-6'>
+            <img src={show?.imageSet.verticalPoster.w240} alt={show?.title} />
+            <StreamingOptions show={show!} country={country!}/>
+        </div>
       </div>
         
     </div>
